a8df46024f517a7366c440a5cab7b60e13c2d84f
pqm03 <qminh203.fw@gmail.com>
2025-04-24 18:12:19 +0700

    Fix: Disable ESLint completely during build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   eslint: {
     // Tắt kiểm tra ESLint trong quá trình build
@@ -19,4 +18,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
