1324bd925f67fbdcc8948f9cc3d4b9d7b1b578ba
pqm03 <qminh203.fw@gmail.com>
2025-04-27 00:14:43 +0700

    Fix: Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -6,12 +6,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  swcMinify: true,
   webpack: (config, { isServer }) => {
     config.ignoreWarnings = [
       { module: /node_modules\/punycode/ }
     ];
     return config;
-  }
+  },
+  output: 'standalone',
+  poweredByHeader: false
 };
 
 module.exports = nextConfig;
